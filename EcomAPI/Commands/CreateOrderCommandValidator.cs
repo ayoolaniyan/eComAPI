88fc9a3be90c4226ce6f724bd16e257f7c906d45
ayobami olaniyan <olaniyanayobamijr@gmail.com>
2026-03-02 08:24:17 +0100

    added Command Validation

---
+++ EcomAPI/Commands/CreateOrderCommandValidator.cs
@@ -0,0 +1,15 @@
+using FluentValidation;
+
+namespace EcomAPI.Commands
+{
+    public class CreateOrderCommandValidator : AbstractValidator<CreateOrderCommand>
+    {
+        public CreateOrderCommandValidator()
+        {
+            RuleFor(x => x.FirstName).NotEmpty();
+            RuleFor(x => x.LastName).NotEmpty();
+            RuleFor(x => x.Status).NotEmpty();
+            RuleFor(x => x.TotalCost).GreaterThan(0);
+        }
+    }
+}
--- EcomAPI/Program.cs
+++ EcomAPI/Program.cs
@@ -1,5 +1,7 @@
+using EcomAPI.Commands;
 using EcomAPI.Data;
 using EcomAPI.Handlers;
+using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,17 +9,28 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlite(builder.Configuration.GetConnectionString("BaseConnection")));
 builder.Services.AddScoped<ICommandHandler<CreateOrderCommand, OrderDto>, CreateOrderCommandHandler>();
 builder.Services.AddScoped<IQueryHandler<GetOrderByIdQuery, OrderDto>, GetOrderByIdQueryHandler>();
+builder.Services.AddScoped<IValidator<CreateOrderCommand>, CreateOrderCommandValidator>();
 
 var app = builder.Build();
 
 app.MapPost("/api/orders", async (ICommandHandler<CreateOrderCommand, OrderDto> handler, CreateOrderCommand command) =>
 {
-    var createdOrder = await handler.HandleAsync(command);
+    try
+    {
+        var createdOrder = await handler.HandleAsync(command);
 
-    if (createdOrder == null)
-        return Results.BadRequest("Failed to create an order");
+        if (createdOrder == null)
+            return Results.BadRequest("Failed to create an order");
 
-    return Results.Created($"/api/orders{createdOrder.Id}", createdOrder);
+        return Results.Created($"/api/orders{createdOrder.Id}", createdOrder);
+        
+    }
+    catch (ValidationException ex)
+    {
+        var errors = ex.Errors.Select(e => new { e.PropertyName, e.ErrorMessage });
+        return Results.BadRequest(errors);
+    }
+    
 });
 
 app.MapGet("/api/orders/{id}", async (IQueryHandler<GetOrderByIdQuery, OrderDto> handler, int id) =>
